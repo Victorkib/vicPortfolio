d0723e73f259420b142cb24e2f4f7d5938dcca75
victorkib <victor3720kibiwott@gmail.com>
2026-05-27 19:57:33 +0300

    vbvb

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -2,6 +2,6 @@ const removeTrailingSlash = (value) => value.replace(/\/+$/, '');
 
 const fallbackApiBaseUrl = 'https://vicportfolio.onrender.com';
 
-const rawApiBaseUrl =  'http://localhost:5000' || import.meta.env.VITE_API_BASE_URL || fallbackApiBaseUrl;
+const rawApiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000' || fallbackApiBaseUrl;
 
 export const API_BASE_URL = removeTrailingSlash(rawApiBaseUrl);
